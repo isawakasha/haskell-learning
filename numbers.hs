59bb3eef1cceb8e54a37079fa189e5d4faeddeae
isawakasha <sort_bagira@mail.ru>
2022-10-20 18:37:44 +0300

    Change SabitNumbers to sabitNumbers and add primeNumbers

--- numbers.hs
+++ numbers.hs
@@ -1,1 +1,5 @@
-SabitNumbers = [3 * 2^n - 1 | n <- [0..]]
+sabitNumbers = [3 * 2^n - 1 | n <- [0..]]
+
+-- Need to figure it out
+primeNumbers = filterPrime [2..]
+  where filterPrime (p:xs) = p : filterPrime [x | x <- xs, x `mod` p /= 0]

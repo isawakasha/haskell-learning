08245e69466dc132933bae0d4919327eab82de56
isawakasha <sort_bagira@mail.ru>
2022-10-20 18:45:02 +0300

    Add factorialNumbers

--- numbers.hs
+++ numbers.hs
@@ -1,5 +1,10 @@
+-- This module is needed to generate sets of numbers
+
 sabitNumbers = [3 * 2^n - 1 | n <- [0..]]
 
--- Need to figure it out
 primeNumbers = filterPrime [2..]
-  where filterPrime (p:xs) = p : filterPrime [x | x <- xs, x `mod` p /= 0]
+  where filterPrime (p:xs) = p : filterPrime [x | x <- xs, x `mod` p /= 0] -- Need to figure it out
+
+factorialNumbers = [product [1..n] | n <- [0..]]
+
+factorial n = factorialNumbers !! n
